aa8cf4d9d1e3c88650eed2892fb68c4abd11dd7f
azer <daoduythai1472001@gmail.com>
2021-11-17 14:27:07 +0700

    update index

--- Product/views.py
+++ Product/views.py
@@ -1,9 +1,23 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from Product.models import *
 
 
 def Index(request):
-    return render(request, 'queenok/index.html')
+    New_product = Product.objects.all().reverse()
+    Women_product = Product.objects.filter(category__name='nu')
+    Men_product = Product.objects.filter(category__name='nam')
+    Fashion_product = Product.objects.filter(tags__name='fashion')
+
+
+    return render(request, 'queenok/index.html',
+                  {'New_product': New_product,
+                   'wm_product': Women_product,
+                   'm_product': Men_product,
+                   'fs_product': Fashion_product
+
+                   }
+                  )
 
 
 def Category(request):
--- Shop/settings.py
+++ Shop/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-print(STATIC_URL)
+
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
